c8e159c5599310361b09dbab2eb9e1f9f87eb8af
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2022-10-25 10:03:54 +0200

    compile and install plink2

--- src/scripts/apt_get_essential.sh
+++ src/scripts/apt_get_essential.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-apt-get update && apt-get install -y --no-install-recommends && \
+apt-get update && apt-get install -y --no-install-recommends \
+    libatlas-base-dev=3.10.3-8ubuntu7 \
+    libgslcblas0=2.5+dfsg-6build1 \
     pandoc=2.5-3build2 \
     pandoc-citeproc=0.15.0.1-1build4 \
+    zlib1g-dev=1:1.2.11.dfsg-2ubuntu1.5 && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
 
