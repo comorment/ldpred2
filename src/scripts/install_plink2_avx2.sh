36dc21940c3a140cdd02b05bb592c26819245814
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2022-10-26 09:31:14 +0000

    remove unused script

--- src/scripts/install_plink2_avx2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-wget --no-check-certificate https://s3.amazonaws.com/plink2-assets/alpha3/plink2_linux_avx2_20220814.zip && \
-    unzip -j plink2_linux_avx2_20220814.zip && \
-    rm -rf plink2_linux_avx2_20220814.zip
-
-cp plink2 /bin
